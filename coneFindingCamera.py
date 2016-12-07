75959c3c6765618ca9268ea6897ee23bf6e143f6
Kimber <kimberly.winter@students.olin.edu>
2016-12-07 18:28:05 -0500

    finds centers

--- coneFindingCamera.py
+++ coneFindingCamera.py
@@ -5,6 +5,8 @@ import time
 import rospy
 from std_msgs.msg import String
 
+practiceImg="./sampleImages/coneline4.jpg"
+
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10) ###Need to figure out where I'm publishing to
     rospy.init_node('Camera', anonymous=True)
@@ -15,19 +17,29 @@ def talker():
         pub.publish(hello_str)
         rate.sleep()
 
-if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+    if __name__ == '__main__':
+        try:
+            talker()
+        except rospy.ROSInterruptException:
+            pass
 
 cap = cv2.VideoCapture(0) #opening webcam
 
-def findCenter():
-    #to do
-    pass
+def findCenter(cnts):
+    centers=[]
+    for c in cnts:
+        if(cv2.arcLength(c, 1)>30):
+        # compute the center of the contour
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            centers.append([cX,cY])
+ 
+    # draw the contour and center of the shape on the image
+            cv2.circle(output, (cX, cY), 7, (255, 255, 255), -1)
+    return centers
 
-def calcGap():
+def calcGap(centers):
     #to do
     pass
 
@@ -35,40 +47,39 @@ def headingCalc():
     #to do
     pass
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read() #gets the frame
-    orangeLower=numpy.array([5, 50, 150], dtype="uint8") #uint8 necessary for this kind of thing 
-    orangeUpper=numpy.array([100, 200, 255], dtype= "uint8") #represents upper and lower bounds of the color "orange"
+#while(True):
+# Capture frame-by-frame
+#ret, frame = cap.read() #gets the frame
+frame=cv2.imread(practiceImg)
+cv2.imshow('Colors', frame) #display filtered out thing
+orangeLower=numpy.array([5, 50, 150], dtype="uint8") #uint8 necessary for this kind of thing 
+orangeUpper=numpy.array([100, 200, 255], dtype= "uint8") #represents upper and lower bounds of the color "orange"
 
-    mask=cv2.inRange(frame, orangeLower,orangeUpper) #creates mask of all the orange pixels
-    output=cv2.bitwise_and(frame,frame,mask=mask) #maps mask onto image
+mask=cv2.inRange(frame, orangeLower,orangeUpper) #creates mask of all the orange pixels
+output=cv2.bitwise_and(frame,frame,mask=mask) #maps mask onto image
 
-    #getting rid of false negatives and other outliers/smoothing over larger objects
-    output = cv2.cvtColor(output, cv2.COLOR_BGR2HSV)
-    output = cv2.erode(output, None, iterations=2)
-    output = cv2.dilate(output, None, iterations=2)
+#getting rid of false negatives and other outliers/smoothing over larger objects
+output = cv2.cvtColor(output, cv2.COLOR_BGR2HSV)
+output = cv2.erode(output, None, iterations=2)
+output = cv2.dilate(output, None, iterations=2)
 
-    #conversion to find contours
-    blurred = cv2.GaussianBlur(output, (7, 7), 0)
-    edged = cv2.Canny(blurred, 50, 150)
+#conversion to find contours
+blurred = cv2.GaussianBlur(output, (7, 7), 0)
+edged = cv2.Canny(blurred, 50, 150)
 
-    # find contours in the edge map
-    contours, hierarchy= cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    #drawing said contours
-    cv2.drawContours(output, contours, -1, (255,0,0),-1)
+# find contours in the edge map
+contours, hierarchy= cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+#drawing said contours
+cv2.drawContours(output, contours, -1, (255,0,0),-1)
+print findCenter(contours)
 
-###Detection part
-    cv2.imshow('Colors', output) #display filtered out thing
-    for row in output:
-        for pixel in row:
-            if pixel is not [0,0,0]:
-               #print "detected!"
-               pass
+###Showing you what happened
+cv2.imshow('Output', output) #display filtered out thing
+cv2.waitKey(0)
 
-    # Display the resulting framegit pu
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# Display the resulting framegit pu
+#if cv2.waitKey(1) & 0xFF == ord('q'):
+ #   break
 
 # When everything done, release the capture
 cap.release()
