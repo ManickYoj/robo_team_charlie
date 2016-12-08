8c6a707db41c8f97fd3648168e294dc8f0748aa1
abuchele <anna.buchele@students.olin.edu>
2016-12-07 21:47:01 -0500

    camera shenanagens

--- coneFindingCamera.py
+++ coneFindingCamera.py
@@ -2,27 +2,27 @@ import cv2
 import numpy
 import time
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Int8MultiArray
 import math
 from operator import itemgetter
 
-practiceImg="./sampleImages/coneline4.jpg"
+PKG = 'numpy_tutorial'
+import roslib; #roslib.load_manifest(PKG)
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10) ###Need to figure out where I'm publishing to
-    rospy.init_node('Camera', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+import rospy
+from rospy.numpy_msg import numpy_msg
+
+practiceImg="./sampleImages/trafficCone.jpg"
+
+def talker(val):
+    pub = rospy.Publisher('camera', Int8MultiArray,queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    r = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
-
-    if __name__ == '__main__':
-        try:
-            talker()
-        except rospy.ROSInterruptException:
-            pass
+        a = Int8MultiArray()
+        a.data = val
+        pub.publish(a)
+        r.sleep()   
 
 cap = cv2.VideoCapture(0) #opening webcam
 
@@ -58,56 +58,56 @@ def calcGap(centers): #finding the largest gap between two centers
 def headingCalc(pt1, pt2, img):
     #because an image is really just a matrix of r, c...
     numColumns=len(img[0])
-    distbWsections=numColumns/7
+    distbWsections=numColumns/11
     #because point1 is closer to the left no matter what, it'll be the lower one
     minSection=pt1[0]/distbWsections+1
-    print minSection
     #to be conservative, compensate by subtracting to get the right section
-    maxSection=(pt2[1]/distbWsections)
-    print maxSection
-    val=[0,0,0,0,0,0,0]
-    for i in range(0,6):
-        if(i<=minSection and i>=maxSection):
+    maxSection=(pt2[0]/distbWsections)
+    val=[0,0,0,0,0,0,0,0,0,0,0]
+    for i in range(0,10):
+        if(i>=minSection and i<=maxSection):
             val[i]=1
+    talker(val)
     return val
 
 
-#while(True):
-# Capture frame-by-frame
-#ret, frame = cap.read() #gets the frame
-frame=cv2.imread(practiceImg)
-cv2.imshow('Colors', frame) #display filtered out thing
-orangeLower=numpy.array([5, 50, 150], dtype="uint8") #uint8 necessary for this kind of thing 
-orangeUpper=numpy.array([100, 200, 255], dtype= "uint8") #represents upper and lower bounds of the color "orange"
+while not rospy.is_shutdown():
+    # Capture frame-by-frame
+    #ret, frame = cap.read() #gets the frame
+    frame=cv2.imread(practiceImg)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #cv2.imshow('Colors', frame) #display filtered out thing
+    orangeLower=numpy.array([5, 42, 160], dtype="uint8") #uint8 necessary for this kind of thing 
+    orangeUpper=numpy.array([120, 120, 255], dtype= "uint8") #represents upper and lower bounds of the color "orange"
 
-mask=cv2.inRange(frame, orangeLower,orangeUpper) #creates mask of all the orange pixels
-output=cv2.bitwise_and(frame,frame,mask=mask) #maps mask onto image
+    mask=cv2.inRange(frame, orangeLower,orangeUpper) #creates mask of all the orange pixels
+    output=cv2.bitwise_and(frame,frame,mask=mask) #maps mask onto image
+    cv2.imshow('Output', output)
+    #getting rid of false negatives and other outliers/smoothing over larger objects
+    output = cv2.cvtColor(output, cv2.COLOR_BGR2HSV)
+    output = cv2.erode(output, None, iterations=2)
+    output = cv2.dilate(output, None, iterations=2)
 
-#getting rid of false negatives and other outliers/smoothing over larger objects
-output = cv2.cvtColor(output, cv2.COLOR_BGR2HSV)
-output = cv2.erode(output, None, iterations=2)
-output = cv2.dilate(output, None, iterations=2)
+    #conversion to find contours
+    blurred = cv2.GaussianBlur(output, (7, 7), 0)
+    edged = cv2.Canny(blurred, 50, 150)
 
-#conversion to find contours
-blurred = cv2.GaussianBlur(output, (7, 7), 0)
-edged = cv2.Canny(blurred, 50, 150)
+    # find contours in the edge map
+    contours, hierarchy= cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    #drawing said contours
+    cv2.drawContours(output, contours, -1, (255,0,0),-1)
+    centers=findCenter(contours)
+    point1, point2=calcGap(centers)
+    headingCalc(point1, point2, output)
+    ###Showing you what happened
+    #cv2.imshow('Output', output) #display filtered out thing
+    cv2.waitKey(0)
 
-# find contours in the edge map
-contours, hierarchy= cv2.findContours(edged,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-#drawing said contours
-cv2.drawContours(output, contours, -1, (255,0,0),-1)
-
-centers=findCenter(contours)
-point1, point2=calcGap(centers)
-headingCalc(point1, point2, output)
-
-###Showing you what happened
-cv2.imshow('Output', output) #display filtered out thing
-cv2.waitKey(0)
-
-# Display the resulting framegit pu
-#if cv2.waitKey(1) & 0xFF == ord('q'):
- #   break
+    # Display the resulting framegit pu
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
 
 # When everything done, release the capture
 cap.release()
